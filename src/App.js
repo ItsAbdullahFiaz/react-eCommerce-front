c467b9bdd5378278af0a6b25e8ed9c384635714e
Abdullah <abdullahfiaz@hwryk.com>
2024-03-10 12:55:11 +0500

    Redux implemented

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Switch, Route } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
@@ -6,10 +6,30 @@ import 'react-toastify/dist/ReactToastify.css'
 import Home from './pages/Home';
 import Login from './pages/auth/Login';
 import Register from './pages/auth/Register';
-import CompleteRegisteration from './pages/auth/RegisterComplete'
 import Header from './components/Header';
+import CompleteRegisteration from './pages/auth/RegisterComplete'
+
+import { auth } from './firebase';
+import { useDispatch } from 'react-redux';
 
 const App = () => {
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(async (user) => {
+      if (user) {
+        const idTokenResult = await user.getIdTokenResult();
+        console.log('user', user);
+        dispatch({
+          type: 'LOGGED_IN_USER',
+          payload: { email: user.email, token: idTokenResult.token },
+        });
+      }
+    });
+    return () => unsubscribe();
+  }, []);
+
   return (
     <>
       <Header />
